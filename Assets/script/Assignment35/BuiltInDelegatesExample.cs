273c9e46bf3dc06bac35195caddb76a3b364a753
shimaa <shimaayasser059@gmail.com>
2025-01-15 14:26:03 +0200

    ..

--- Assets/script/Assignment35/BuiltInDelegatesExample.cs
+++ Assets/script/Assignment35/BuiltInDelegatesExample.cs
@@ -1,7 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
+namespace Assignment_35
+{
 public class BuiltInDelegatesExample : MonoBehaviour
 {
+    void Start(){
+        
+        Action logMessage =()=>Debug.Log($"Hello from Action delegate!");
+        logMessage();
+       
+        Func<int, int>square =number=>number*number;
+        Debug.Log($"Square: {square(5)}");
+       
+        Predicate<int> isEven =number=>number%2==0;
+        Debug.Log($"Is 4 even? {isEven(4)}");
+
+    }
+}
 }
